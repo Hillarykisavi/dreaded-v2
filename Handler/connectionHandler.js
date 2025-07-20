0919fd4a09f3c3275c7271c3b86e78da85046bfa
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-07-20 10:57:02 +0300

    Update connectionHandler.js

--- Handler/connectionHandler.js
+++ Handler/connectionHandler.js
@@ -83,8 +83,7 @@ const connectionHandler = async (client, update, startDreaded) => {
 
   if (connection === "open") {
 
-const res = await client.newsletterFollow('120363420158562302@newsletter');
-console.log(res);
+
 
     if (database) {
       console.log("📈 Connecting to PostgreSQL database...");
