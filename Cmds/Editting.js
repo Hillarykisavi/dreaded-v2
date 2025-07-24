f9fdb90a2612d1560d5d174cccc9efeb1fb53236
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-07-24 21:54:37 +0300

    Update Editting.js

--- Cmds/Editting.js
+++ Cmds/Editting.js
@@ -428,37 +428,7 @@ dreaded({
 });
 
 
-dreaded({
-  pattern: "toimg",
-  desc: "Toimg command",
-  category: "Editting",
-  filename: __filename
-}, async ({ client, m, mime, exec, getRandom }) => {
-  
-  
-  
-  m.reply("command is in error");
-  
-  /* try {
-  
-  if (!m.quoted) return m.reply('Tag a static video with the command!'); 
-      if (!/webp/.test(mime)) return m.reply(`Tag a sticker with the command`); 
-  
-      let media = await client.downloadAndSaveMediaMessage(m.quoted); 
-      let mokaya = await getRandom('.png'); 
-      exec(`ffmpeg -i ${media} ${mokaya}`, (err) => { 
-     fs.unlinkSync(media); 
-     if (err) m.reply(err)
-     let buffer = fs.readFileSync(mokaya); 
-     client.sendMessage(m.chat, { image: buffer, caption: `Converted by Dreaded! 🦄`}, { quoted: m}) 
-     fs.unlinkSync(mokaya); 
-      }); 
-  
-  } catch (e) {
-   m.reply('I am unable to convert animated stickers')}
-  
-  */
-});
+
 
 
 dreaded({
