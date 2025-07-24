b1745942f657c9f1756359d51358d797d5797378
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-07-24 22:10:34 +0300

    Update Conversion.js

--- Cmds/Conversion.js
+++ Cmds/Conversion.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const ffmpeg = require("fluent-ffmpeg");
 const { tmpdir } = require("os");
 const path = require("path");
+const { exec } = require("child_process");
 
 dreaded({
   pattern: "toaudio",
