a62e262f634b422f1b3f1255b26b753c9c1c5051
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-07-24 19:27:59 +0300

    Update General.js

--- Cmds/General.js
+++ Cmds/General.js
@@ -381,30 +381,36 @@ alias: ["ranime"],
 dreaded({
   pattern: "retrieve",
   desc: "Retrieve command",
-alias: ["vv"],
+  alias: ["vv"],
   category: "General",
   filename: __filename
 }, async (context) => {
-  
-          const { client, m } = context;
-  
-  
-  
-  if (!m.quoted) return m.reply("quote a viewonce message eh")
-  
-    const quotedMessage = m.msg?.contextInfo?.quotedMessage;
-  
-      if (quotedMessage.imageMessage) {
-        let imageCaption = quotedMessage.imageMessage.caption;
-        let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
-        client.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
-      }
-  
-      if (quotedMessage.videoMessage) {
-        let videoCaption = quotedMessage.videoMessage.caption;
-        let videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
-        client.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
-      }
+  const { client, m } = context;
+
+  if (!m.quoted) return m.reply("quote a viewonce message eh");
+
+  const quotedMessage = m.msg?.contextInfo?.quotedMessage;
+
+  if (quotedMessage.imageMessage) {
+    let imageCaption = quotedMessage.imageMessage.caption;
+    let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+    client.sendMessage(m.chat, { image: { url: imageUrl }, caption: imageCaption });
+  }
+
+  if (quotedMessage.videoMessage) {
+    let videoCaption = quotedMessage.videoMessage.caption;
+    let videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+    client.sendMessage(m.chat, { video: { url: videoUrl }, caption: videoCaption });
+  }
+
+  if (quotedMessage.audioMessage) {
+    let audioUrl = await client.downloadAndSaveMediaMessage(quotedMessage.audioMessage);
+    client.sendMessage(m.chat, {
+      audio: { url: audioUrl },
+      mimetype: 'audio/ogg; codecs=opus',
+      ptt: true
+    });
+  }
 });
 
 
